d9278aac868c17c5d654ab94bbcbea42414a9fc5
Daniel Coelho <dpcsoftware@gmail.com>
2017-07-02 21:27:15 -0300

    Requesting storage permission in the EditPreferences activity
    Checking storage permission before proceeding to auto backup

--- app/src/main/java/com/dpcsoftware/mn/EditPreferences.java
+++ app/src/main/java/com/dpcsoftware/mn/EditPreferences.java
@@ -19,18 +19,63 @@
 
 package com.dpcsoftware.mn;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
-
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 
 public class EditPreferences extends PreferenceActivity {
-	
+    private CheckBoxPreference autoBackupPref;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {    
 	    super.onCreate(savedInstanceState);
 	    addPreferencesFromResource(R.xml.prefs);
+
+        autoBackupPref = (CheckBoxPreference) findPreference("BACKUP_AUTO");
+        autoBackupPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object obj) {
+                if ((Boolean)obj) {
+                    // Check storage permission
+                    int permission = ContextCompat.checkSelfPermission(EditPreferences.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    if (permission == PackageManager.PERMISSION_GRANTED) {
+                        return true;
+                    }
+                    else {
+                        ActivityCompat.requestPermissions(EditPreferences.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                        return false;
+                    }
+                }
+                else {
+                    return true;
+                }
+            }
+        });
 	    
 	    setTitle(R.string.editpreferences_c1);
 	}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            int i;
+            for (i = 0; i < permissions.length; ++i) {
+                if (permissions[i].equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        autoBackupPref.setChecked(true);
+                    }
+                }
+            }
+        }
+    }
 }
