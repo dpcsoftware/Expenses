f517c2df25c58b7d2a9c878ae38336a17d8187dd
Daniel Coelho <dpcsoftware@gmail.com>
2023-01-05 21:54:19 -0300

    Ask for POST_NOTIFICATIONS permission in Android API 33 or higher

--- app/src/main/java/com/dpcsoftware/mn/EditPreferencesFragment.java
+++ app/src/main/java/com/dpcsoftware/mn/EditPreferencesFragment.java
@@ -25,23 +25,49 @@ import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.preference.CheckBoxPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class EditPreferencesFragment extends PreferenceFragmentCompat {
-    private CheckBoxPreference autoBackupPref;
-    private ActivityResultLauncher<String> permissionLauncher;
+    private CheckBoxPreference autoBackupPref, budgetAlertsPref;
+    private ActivityResultLauncher<String> permissionStorageLauncher, permissionNotificationsLauncher;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.prefs, rootKey);
 
-        if (!App.isAndroidQOrAbove()) {
+        if (App.isAndroidQOrAbove()) {
+            budgetAlertsPref = findPreference("budgetAlert");
+
+            permissionNotificationsLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+                    isGranted -> {
+                        if (isGranted) {
+                            budgetAlertsPref.setChecked(true);
+                        }
+                    }
+            );
+
+            budgetAlertsPref.setOnPreferenceChangeListener((preference, obj) -> {
+                if ((Boolean) obj) {
+                    // Check notifications permission
+                    int permission = ContextCompat.checkSelfPermission(requireContext(),
+                            Manifest.permission.POST_NOTIFICATIONS);
+                    if (permission == PackageManager.PERMISSION_GRANTED) {
+                        return true;
+                    } else {
+                        permissionNotificationsLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+                        return false;
+                    }
+                } else {
+                    return true;
+                }
+            });
+        }
+        else {
             autoBackupPref = findPreference("BACKUP_AUTO");
 
-            permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+            permissionStorageLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
                     isGranted -> {
                         if (isGranted) {
                             autoBackupPref.setChecked(true);
@@ -57,7 +83,7 @@ public class EditPreferencesFragment extends PreferenceFragmentCompat {
                     if (permission == PackageManager.PERMISSION_GRANTED) {
                         return true;
                     } else {
-                        permissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                        permissionStorageLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
                         return false;
                     }
                 } else {
